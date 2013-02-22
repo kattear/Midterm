794f3fbbc03a56a4b406e623f67ddbaa1bef4479
Katherine Tear <kathtear@gmail.com>
2013-02-22 16:50:17 -0500

    attempt at php

---
+++ public.html/includes/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<title>KAT Advanced Web</title>
+		<link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico">
+		<link href='http://fonts.googleapis.com/css?family=EB+Garamond' rel='stylesheet' type='text/css'>
+		<link href="css/style.css" rel="stylesheet" />
+	</head>
+	
+	<body>
---
+++ public.html/pages/home.php
@@ -0,0 +1,31 @@
+		<article id="wrapper">
+			<header>
+				<h1><a href=""><img src="images/k@.png" alt="K@"></a></h1>
+			</header>
+			
+			<nav class="lavan">
+				<ul>
+					<li><a href="">Lorem Ipsum</a>
+					<li><a href="">Lorem Ipsum</a>
+					<li><a href="">Lorem Ipsum</a>
+					<li><a href="">Lorem Ipsum</a>
+					<li><a href="">Lorem Ipsum</a>
+					<div class="lava"></div>
+				</ul>
+			</nav>
+			
+			<article>
+				<section class="backed">
+					<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sem augue, mollis quis venenatis eu, suscipit sed metus. Suspendisse vehicula, ante ac auctor sodales, magna nunc ultrices lacus, rhoncus ullamcorper mauris nibh eget neque. Proin eu dolor quam, et laoreet sapien. Curabitur auctor tristique mauris eget aliquam. Vivamus lobortis tortor eu neque dictum facilisis. Sed malesuada leo sed nisi ultricies placerat. Nam at leo sapien, id euismod nulla. Mauris accumsan fermentum nisl, et vehicula enim bibendum non.
+					</p>
+
+					<p>
+					Maecenas in quam arcu, sit amet faucibus tellus. In nibh lectus, porttitor eu accumsan quis, ultricies non nulla. Mauris mi ligula, cursus eu consectetur quis, adipiscing vel elit. Vestibulum commodo, velit eu posuere sagittis, urna urna auctor eros, vel dignissim lorem tortor vitae mi. Nulla tempus magna tincidunt mauris tristique porta. Praesent ornare nisl sit amet orci fermentum at pretium nunc mattis. Morbi varius, eros at lacinia tristique, ligula massa pellentesque risus, eu sollicitudin risus massa non ante. Nulla pretium venenatis nisl vitae sodales.
+					</p>
+				</section>
+				
+				<section id="reese" class="imgs">
+					<img src="images/Reese.jpg" alt="Reese" title="Reese"/>
+				</section>
+			</article>
